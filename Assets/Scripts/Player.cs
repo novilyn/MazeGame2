d74ce3507bdb2727d68167c61f90af8d39c63341
novilyn <43879980+novilyn@users.noreply.github.com>
2018-11-07 19:56:21 +0200

    Class 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,8 @@ public class Player : MonoBehaviour {
 	// Members of variables
 	public float moveSpeed = 7.0f;
 	
-	
+	public GameObject bulletSpawner;
+	public GameObject bullet;
 	// Methods or functions
 	
 	
@@ -14,8 +15,30 @@ public class Player : MonoBehaviour {
 	void Update () {
 		//Player movement
 		if(Input.GetKey(KeyCode.W))
-	{
-			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		{
+				transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+				//transform.GetChild(0).LookAt();
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+				transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+				transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Shoot();
+		}
 	}
+	void Shoot(){
+		Instantiate(bullet.transform, bulletSpawner.transform.position,bulletSpawner.transform.rotation);
+	
 	}
+	
 }
